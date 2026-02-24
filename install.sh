92bece9f318d602c354e3557e8a3e7814508cf72
Jacob Roberts <jroberts.g1ql8@simplelogin.com>
2026-02-24 13:40:39 +0000

    Move uwsm to omarchy

--- install.sh
+++ install.sh
@@ -2,14 +2,14 @@
 
 # if stow is installed, use it to install the dotfiles
 if command -v stow >/dev/null 2>&1; then
-    stow --no-folding ghostty
+    #stow --no-folding ghostty
     stow --no-folding git
     stow --no-folding nvim
     stow --no-folding opencode
     stow --no-folding ssh
-    stow --no-folding uwsm
-    stow --no-folding vscodium
-    stow --no-folding zed
+    #stow --no-folding uwsm
+    #stow --no-folding vscodium
+    #stow --no-folding zed
     stow --no-folding zsh
 else
     echo "stow is not installed"
